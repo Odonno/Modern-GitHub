47d843fd016c149a67f280d7bd82295869438629
Odonno <bottiau.david@laposte.net>
2014-11-29 22:44:13 +0100

    Design the empty body of Main of the WP app

--- GitHub/GitHub/GitHub.Shared/App.xaml.cs
+++ GitHub/GitHub/GitHub.Shared/App.xaml.cs
@@ -109,7 +109,7 @@ namespace GitHub
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(SplashScreenPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
