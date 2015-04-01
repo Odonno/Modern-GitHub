a02b1a06d418935879f69f8265135ba98da496df
Odonno <bottiau.david@laposte.net>
2015-04-01 22:51:14 +0200

    Remove contact info

--- GitHub/GitHub/GitHub.Shared/ViewModel/Abstract/IMainViewModel.cs
+++ GitHub/GitHub/GitHub.Shared/ViewModel/Abstract/IMainViewModel.cs
@@ -13,7 +13,5 @@ namespace GitHub.ViewModel.Abstract
 
         ICommand ToggleEnableSearchCommand { get; }
         ICommand RefreshCommand { get; }
-        ICommand ShareIdeaCommand { get; }
-        ICommand ContactSupportCommand { get; }
     }
 }
